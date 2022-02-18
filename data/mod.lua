c27d82eed20a319b12470d867b37a5ed602ea519
nmslq <96362104+nmslq@users.noreply.github.com>
2022-02-18 21:36:11 +0800

    beta update 3

--- data/mod.lua
+++ data/mod.lua
@@ -6,7 +6,8 @@ end
 
 function onUpdate()
    if mods.have then
-     mod = create.mod.text(0, 0, [mod.name ]loaded, 30, sans, [have.mods.maths])
+     mod = create.mod.text(0, 0, [mod.name]loaded, 30, sans, [have.mods.maths])
+     version = create.version.text(-30, -30, version:[mod.version], 30, sans
    end
    if mods.nothave then
      not = create.not.text(0, 0, there isn't have any mod!, 30, sans
