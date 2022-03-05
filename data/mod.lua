94ae24c846ab77d97f8b4ae8a065bf74482e89eb
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-05 16:16:53 +0800

    mod sys

--- data/mod.lua
+++ data/mod.lua
@@ -7,6 +7,26 @@ end
 
 function onUpdate()
    controls.Update()
+   if mods.folder ishave then
+      if not have mod then
+        setRoom(modRoom)
+      end
+      if have mod then
+        setRoom(modRoom)
+        load(mods/.mod)
+        load(mods/.mod/allfiles)
+        load(mods/.mod/allfolder)
+        if load done then
+          start.mod()
+            end
+        end
+    end
+    if mods.folder nothave then
+      create.folder(mods)
+      create.files(readme.txt)
+      set.files(readme.txt: put mods in here)
+      reload(mods.folder)
+    end
    if mods.have then
      setRoom(modsRoom.have)
      mod = create.mod.text(0, 0, [mod.name]loaded, 30, sans, [have.mods.maths])
