6caf2aa22505e720da81b0705b48f35c6aed1dc1
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-13 18:23:04 +0800

    Update stages.lua

--- data/libraries/stages.lua
+++ data/libraries/stages.lua
@@ -1,9 +1,9 @@
 function stages()
    if stage = 1 then
-     gr = create.gr.sprite(0,-100,gr)
+     gr = create.gr.sprite(0,-100,ground)
      gr.scale(1.6, 1.6)
      gr.scoll(1, 1)
-     pf1 = create.pf1.sprite(-200,100,pf1)
+     pf1 = create.pf1.sprite(-200,100,platform1)
      pf1.scale(1.3,1.3)
      pf.scoll(1,1)
      sky = create.sky.sprite(0,0,sky)
