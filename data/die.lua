e9a01b952c317e6274b99269c624aa8ca1ac5c29
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-15 12:48:10 +0800

    Update die.lua

--- data/die.lua
+++ data/die.lua
@@ -1,18 +1,24 @@
 function firstDie()
    game = create.game.sprite(0, -150, game, 50, 30)
+   game.alpha = 0
+   game.alpha = game.alpha + 1
    over = create.game.sprite(0, -100, over, 50, 30)
+   over.alpha = 0
+   over.alpha = over.alpha + 1
    fd = create.fd.text(0, 0, you died!, 30, sans)
    fd1 = create.fd1.text(0, 0, dont give up, 30, sans, 0)
 end
 
 function Die()
    game = create.game.sprite(0, -150, game, 50, 30)
+   game.alpha = 0
+   game.alpha = game.alpha + 1
    over = create.game.sprite(0, -100, over, 50, 30)
+   over.alpha = 0
+   over.alpha = over.alpha + 1
    die = create.die.text(0, 0, ..., 30, sans)
 end
 
 function onPress()
-   pr.fd(next fd1)
-   pr.die()
-   fd.fie = room.game.1
+   setRoom(menu)
 end
