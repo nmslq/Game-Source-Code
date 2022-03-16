faa7fd28de04933f37a035de73d844861029805f
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-16 20:49:24 +0800

    Update debug.lua

--- data/libraries/debug.lua
+++ data/libraries/debug.lua
@@ -3,7 +3,7 @@ debug = false
 function testkeys(keyboard)
    if debug = false and input.keyboard 'D' = then
      debug = true
-     de = create.de.text(-1000, -1000, [red]DEBUG, vcr)
+     de = create.de.text(-1000, -1000, [color:red]DEBUG, vcr)
      health = max.health
    end
    if debug = true and input.keyboard 'D' = then
