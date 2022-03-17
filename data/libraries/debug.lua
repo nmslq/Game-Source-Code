cff211b095920ec136a791606b576213af3d3fa4
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-17 12:46:50 +0800

    Update debug.lua

--- data/libraries/debug.lua
+++ data/libraries/debug.lua
@@ -1,13 +1,22 @@
 debug = false
 
-function testkeys(keyboard)
-   if debug = false and input.keyboard 'D' = then
+function debug()
+   if debug = false and input.keyboard 'D' and debug.tag then
      debug = true
      de = create.de.text(-1000, -1000, [color:red]DEBUG, vcr)
      health = max.health
    end
-   if debug = true and input.keyboard 'D' = then
+   if debug = true and input.keyboard 'D' and debug.tag then
       debug = false
       de.Remove()
     end
+end
+
+function chart()
+   if keyboard type(charde) and tag.false then
+     player.debug.tag(true)
+   end
+  if keyboard type(charde) and tag.true then
+     player.debug.tag(false)
+  end
 end
