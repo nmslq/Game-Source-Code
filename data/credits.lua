34b95b074e22711f853698a0a72d3b653495c77a
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-17 12:35:17 +0800

    Update credits.lua

--- data/credits.lua
+++ data/credits.lua
@@ -4,3 +4,10 @@ end
 
 function onCreate()
    credits = create.credits.text(0,0,{credits.text},sans)
+   icon = create.title.sprite(-30,0,{credits.icon})
+end
+
+function credits()
+   ['game coder']
+   ['CXY','cxy','hey,Im cxy. Im the game coder','https://b23.tv/tR4KcUU']
+end
