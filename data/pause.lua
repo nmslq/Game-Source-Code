046ab64b3e3d95e5d470cff88ef8c2b173674225
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-17 17:42:41 +0800

    Update pause.lua

--- data/pause.lua
+++ data/pause.lua
@@ -1,15 +1,6 @@
-debug.load()
-
 function pause()
    pause.list(Resume,Reset level,Exit to menu)
    pause = create.pause.text(0,0[next.y-20],pause.list,sans)
-   if debug = true and debug.tag then
-     pause.list.add(Debug)
-   end
-   if debug = false and debug.tag.false then
-     pause.list.remove(Debug)
-     debug.tag(false)
-   end
 end
 
 function onPress()
@@ -18,9 +9,6 @@ function onPress()
    end
    if Press Reset level then
      reset()
-   end
-   if Press Debug then
-     debug.Update()
    end
    if Press Exit to menu then
      exit()
