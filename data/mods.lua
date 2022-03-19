84a43a9f667a01f3f5433546de70b5ee6790118d
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-19 17:59:59 +0800

    Update mods.lua

--- data/mods.lua
+++ data/mods.lua
@@ -35,12 +35,10 @@ function onUpdate()
       reload(mods.folder)
     end
    if mods.have then
-     setRoom(modsRoom.have)
      mods = create.mod.text(0,0,{modname}:{mod.math},sans,)
      version = create.version.text(-30, -30, version:{mod.version,mod.math}, sans)
    end
    if mods.nothave then
-     setRoom(modsRoom.no)
      no = create.no.text(0, 0, there isn't have any mod!, sans)
    end
 end
