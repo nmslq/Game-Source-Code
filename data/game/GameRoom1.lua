0e4afa0c3f375597d43046d79b9efbaa78fc2d30
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-20 16:25:00 +0800

    Create GameRoom1.lua

---
+++ data/game/GameRoom1.lua
@@ -0,0 +1,30 @@
+controls.load()
+fight.load()
+timer.load()
+shaders.cam()
+stage = 1
+
+function inRoom()
+   nameRoom(game.1)
+end
+
+function onUpdate()
+  controls.Update()
+  fight.Update()
+  shaders.Update()
+  timer.Update()
+end
+
+function summon()
+   if time % 13 == 0 then
+      rx = math.random(stage.x)
+      ry = math.random(stage.y)
+      summon.bullet(rx,ry)
+   end
+end
+
+function addControls()
+   if Android then
+      Add.controls(full, B, C, S)
+   end
+end
