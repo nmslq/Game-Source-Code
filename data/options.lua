86d738be1e170c51abd5d7bcc46f98efc4f590f6
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-20 15:26:42 +0800

    Update options.lua

--- data/options.lua
+++ data/options.lua
@@ -10,4 +10,22 @@ function onCreate()
    if not debug then
      options.list.remove(Debug Setting)
    end
-   
+   options = create.options.text(0,0[next.y-20],options.list,sans)
+end
+
+function onPress()
+   if Press Controls Setting then
+     black.screen()
+     setRoom(controls setting)
+   end
+   if Press Game Setting then
+     black.screen()
+     setRoom(game setting)
+   end
+   if debug then
+      if Press Debug Setting then
+        black.screen()
+        setRoom(debug setting)
+      end
+   end
+end
