0f3349caeb2082f0c002a7403a5f32fcea86f663
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-20 16:51:56 +0800

    ok

--- data/options/ControlsSetting.lua
+++ data/options/ControlsSetting.lua
@@ -3,4 +3,12 @@ function inRoom()
 end
 
 function setting()
+   controls.up.text(keyboard.change)
+   controls.left.text(keyboard.change)
+   controls.right.text(keyboard.change)
+   controls.down.text(keyboard.change)
+   controls.A.text(keyboard.change)
+   controls.B.text(keyboard.change)
+   controls.C.text(keyboard.change)
+   controls.S.text(keyboard.change)
 end
