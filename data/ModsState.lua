87f064d2d432a7dbcf7d097b159966e3fc89f215
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-23 20:48:20 +0800

    Update ModsState.lua

--- data/ModsState.lua
+++ data/ModsState.lua
@@ -1,4 +1,3 @@
-mods = not
 controls.load()
 shaders.cam()
 
@@ -15,39 +14,15 @@ end
 function onCreate()
    bg = create.bg.sprite(0,0,menu)
    bg.scale(2.3,2.3)
+   if mod.paths.have then
+     mods = create.mod.text(0,0,{modname}:{mod.math},sans)
+     version = create.version.text(-30, -30, version:{mod.version,mod.math}, sans)
+   end
+   if mod.paths.nothave then
+     no = create.no.text(0, 0, there isn't have any mod!, sans)
+   end
 end
 
 function onUpdate()
    controls.Update()
-   if mods.folder ishave then
-      if not have mod then
-        setRoom(modRoom)
-      end
-      if have mod then
-        setRoom(modRoom)
-        load(mods/.mod)
-        load(mods/.mod/data)
-        load(mods/.mod/images)
-        load(mods/.mod/fonts)
-        load(mods/.mod/music)
-        load(mods/.mod/sounds)
-        load(mods/.mod/videos)
-        if load done then
-          start.mod()
-            end
-        end
-    end
-    if mods.folder nothave then
-      create.folder(mods)
-      create.files(readme.txt)
-      set.files(readme.txt: put mods in here)
-      reload(mods.folder)
-    end
-   if mods.have then
-     mods = create.mod.text(0,0,{modname}:{mod.math},sans,)
-     version = create.version.text(-30, -30, version:{mod.version,mod.math}, sans)
-   end
-   if mods.nothave then
-     no = create.no.text(0, 0, there isn't have any mod!, sans)
-   end
 end
