a87b0d07dd8908824d3b639c997add4643a91d52
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-25 17:55:11 +0800

    Update Tools.lua

--- data/libraries/Tools.lua
+++ data/libraries/Tools.lua
@@ -1,25 +1,33 @@
 function tools()
    flx.tools()
+   flx.files()
+   flx.path.files()
    windows.reload()
    android.reload()
    if save then
       if android then
-        save.android.sys.files(androidname)
-        files.create.android(androidname)
+        save.android.sys.files(name)
+        files.sys.exites(name)
+        files.create.android(name)
         end
        if windows then
-         windows.files(windowsname)
-         windows.create.files(windowsname)
+         windows.files.save(name)
+         windows.files.exites(name)
+         windows.create.files(name)
        end
    end
    if edit then
       if android then
         load.android.files.name()
+        getPath.exites(files)
+        files.sys.exites(files)
         change.android.files()
         edit.files(android)
       end
       if windows then
         load.windows.files.name()
+        getPath.exites(files)
+        files.sys.exites(files)
         change.windows.files()
         edit.files(windows)
       end
@@ -27,28 +35,35 @@ function tools()
    if open then
       if windows then
         windows.sys.open(win)
+        windows.sys.flx(window)
       end
       if android then
         android.sys.open(win)
+        android.sys.flx(window)
       end
    end
    if close then
       if windows then
         windows.sys.close(win)
+        windows.sys.flx(window)
       end
       if android then
         android.sys.close(win)
+        android.sys.flx(window)
       end
    end
    if exit then
       if windows then
         windows.close.game()
+        windows.sys.close()
       end
       if android then
         android.app.close()
+        android.app.sys()
       end
    end
    if window.size then
+     windows.sys.flx(window)
      size.window()
      size.slow()
      size.fast()
@@ -57,9 +72,11 @@ function tools()
    if win then
       if windows then
         change.open.windows(tag,text)
+        windows.sys.flx(window)
       end
       if android then
         change.open.android(tag,text)
+        android.sys.flx(window)
       end
    end
    if not files then
