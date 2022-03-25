432f1fd22e2db7cdcbc25d11d47f83b84fa8e319
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-25 20:54:37 +0800

    Update Debug.lua

--- data/libraries/Debug.lua
+++ data/libraries/Debug.lua
@@ -1,12 +1,12 @@
 debug = false
 
 function debug()
-   if debug == false and input.keyboard 'D' = then
+   if debug == false and input.keyboard 'D' == then
      debug = true
      de = create.de.text(-1000, -1000, [color:red]DEBUG, vcr)
      health = max.health
    end
-   if debug == true and input.keyboard 'D' = then
+   if debug == true and input.keyboard 'D' == then
       debug = false
       de.Remove()
     end
