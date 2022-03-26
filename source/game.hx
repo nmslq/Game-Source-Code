7f71971bcbdd7b64c8dd15fff37ab121b64e9e6f
nmslq <96362104+nmslq@users.noreply.github.com>
2022-03-26 11:26:25 +0800

    Update game.hx

--- source/game.hx
+++ source/game.hx
@@ -1,8 +1,13 @@
 function onGameStart()
    if Windows then
      set.win.name(game)
+     flx.getPath(files)
+     getPath.exites()
    end
    if Android then
      clean.sys()
+     sys.rePath()
+     android.flx.getPath(files)
+     flx.getPath.exites()
    end
 end
