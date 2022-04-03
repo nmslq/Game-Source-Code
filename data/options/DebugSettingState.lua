af1aa9eb196930c30dbaca2884aaeae0a39736af
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-03 20:53:16 +0800

    Update DebugSettingState.lua

--- data/options/DebugSettingState.lua
+++ data/options/DebugSettingState.lua
@@ -1,3 +1,17 @@
+controls.load()
+shaders.camera()
+
+function onUpdate()
+   controls.Update()
+   shaders.Update()
+end
+
+function addControls()
+   if Android then
+     Add.controls(up,down,A,B)
+   end
+end
+
 function setting()
    setting.list(FPS Show,Player XY)
    fps.show.list(false,true)
