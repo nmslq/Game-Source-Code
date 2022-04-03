ebb3b1a32337e42eaa21fab0bc79552f05b4f138
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-03 21:36:34 +0800

    Update Shaders.lua

--- data/libraries/Shaders.lua
+++ data/libraries/Shaders.lua
@@ -4,25 +4,35 @@ FlxG.camera()
 FlxG.shaders()
 
 function shaders()
-   if camera.move then
-     camera.move.Flx(x,y)
+   if move.camera then
+     move.camera.Flx(x,y)
+     move.camera.FlxG(x,y)
+     move.camera(x,y)
    end
    if camera.rotation then
      camera.rotation.Flx(index)
+     camera.rotation.FlxG(index)
+     camera.rotation(index)
    end
    if set.camera then
-     camera.set()
+     camera.set.Flx()
+     camera.set.FlxG()
      Flx.camera('hud')
      FlxG.camera('hud')
      Flx.camera('game')
      FlxG.camera('game')
+     camera.set()
    end
    if camera.scale then
      camera.scale.Flx(index)
-     scale.shader()
+     camera.scale.FlxG(index)
+     scale.shader.Flx()
+     scale.shader.FlxG()
    end
    if camera.flash then
      camera.Flx()
+     camera.FlxG()
      flash.Flx(color,time)
+     flash.FlxG()
    end
 end
