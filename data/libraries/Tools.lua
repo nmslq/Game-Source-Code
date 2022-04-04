2a4322af93c4260581456fa7bb8906945b8f0d68
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-04 19:39:54 +0800

    Update Tools.lua

--- data/libraries/Tools.lua
+++ data/libraries/Tools.lua
@@ -6,90 +6,102 @@ function tools()
    android.reload()
    if save then
       if android then
-        save.android.sys.files(name)
-        files.sys.exites(name)
-        files.create.android(name)
+        save.android.systemFiles('files')
+        files.system.exites('files')
+        files.create.android('files')
         end
        if windows then
-         windows.files.save(name)
-         windows.files.exites(name)
-         windows.create.files(name)
+         windows.files.save('files')
+         windows.files.exites('files')
+         windows.create.filesExites('files')
        end
    end
    if edit then
       if android then
         load.android.files.name()
-        getPath.exites(files)
-        files.sys.exites(files)
+        files.getPath.exites('files')
+        files.system.exites('files')
         change.android.files()
-        edit.files(android)
+        edit.files('android')
       end
       if windows then
         load.windows.files.name()
-        getPath.exites(files)
-        files.sys.exites(files)
+        files.getPath.exites('files')
+        files.system.exites('files')
         change.windows.files()
-        edit.files(windows)
+        edit.files('windows')
       end
    end
    if open then
       if windows then
-        windows.sys.open(win)
-        windows.sys.flx(window)
+        windows.system.open('lime')
+        windows.system.Flx('window')
+        windows.system.FlxG('window')
       end
       if android then
-        android.sys.open(win)
-        android.sys.flx(window)
+        android.system.open(win)
+        android.system.Flx(window)
+        android.system.FlxG('window')
       end
    end
    if close then
       if windows then
-        windows.sys.close(win)
-        windows.sys.flx(window)
+        windows.system.close('lime')
+        windows.system.FlxClose('window')
+        windows.system.FlxG('window')
       end
       if android then
-        android.sys.close(win)
-        android.sys.flx(window)
+        android.system.close('lime')
+        android.system.FlxClose('window')
+        android.system.FlxG('window')
       end
    end
    if exit then
       if windows then
         windows.close.game()
-        windows.sys.close()
+        windows.system.close()
+        window.exit.system()
       end
       if android then
         android.app.close()
-        android.app.sys()
+        android.app.system()
+        android.exit.system()
       end
    end
    if window.size then
-     windows.sys.flx(window)
+     windows.system.Flx('window')
+     winows.system.FlxG('window')
      size.window()
      size.slow()
      size.fast()
-     window.resize()
+     Flx.window.resize()
+     FlxG.window.resize()
    end
-   if lime then
+   if limeText then
       if windows then
-        change.open.windows(tag,text)
-        windows.sys.flx(window)
+        windows.system.lime('tag','text')
+        windows.system.windowText('tag','text')
+        windows.system.Flx('window')
+        windows.system.FlxG('window')
       end
       if android then
-        change.open.android(tag,text)
-        android.sys.flx(window)
+        android.system.lime('tag','text')
+        android.system.windowText('tag',text')
+        android.system.Flx('window')
+        android.system.FlxG('window')
       end
    end
    if not files error then
       if android then
         android.tools.open('error')
-        android.tools.lime('error','error {files.name}')
+        android.tools.limeText('error','error {files.name}')
         android.tools.save('log.txt')
         android.tools.edit('log','error in {files.name}')
         exit.game.tools()
       end
       if windows then
         windows.tools.open('error')
-        windows.tools.lime('error','error {files.name}')
+        windows.tools.limeText('error','error {files.name}')
         windows.tools.save('log.txt')
         windows.tools.edit('log',"error in {files.name}')
         exit.game.tools()
