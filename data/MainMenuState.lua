dae3af2d4e06e3d2997a643894875a81b5dda128
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-04 13:26:34 +0800

    Update MainMenuState.lua

--- data/MainMenuState.lua
+++ data/MainMenuState.lua
@@ -3,18 +3,18 @@ tools.load()
 shaders.camera()
 
 function inRoom()
-   nameRoom(menu)
+   nameRoom('menu')
 end
 
 function onCreate()
-   title = create.title.sprite(0,100,title)
+   title = create.title.sprite(0, 100, 'title')
    title.scale(1.5,1)
-   version = create.version.text(-300,-300,version:{getBuildVersion},sans)
-   start = create.start.text(0,30,start,sans)
-   mods = create.mods.text(0,0,mods,sans)
-   credits = create.credits.text(0,-30,credits,sans)
-   options = create.mods.text(0,-60,options,sans)
-   exit = create.exit.text(0,-90,exit,sans)
+   version = create.version.text(-300, -300, 'version:{getBuildVersion}','sans')
+   start = create.start.text(0, 30, 'start', 'sans')
+   mods = create.mods.text(0, 0, 'mods', 'sans')
+   credits = create.credits.text(0, -30, 'credits', 'sans')
+   options = create.mods.text(0, -60, 'options' ,'sans')
+   exit = create.exit.text(0, -90, 'exit', 'sans')
 end
 
 function onUpdate()
@@ -26,19 +26,19 @@ end
 function onPress()
    if Press start then
      black.screen()
-     setRoom(game.1)
+     setRoom('game.1')
    end
    if Press mods then
      black.screen()
-     setRoom(mods)
+     setRoom('mods')
    end
    if Press credits then
      black.screen()
-     setRoom(credits)
+     setRoom('credits')
    end
    if Press options then
      black.screen()
-     setRoom(options)
+     setRoom('options')
    end
    if Press exit then
      exit.game.tools()
