8fd05b1089049c54fc273ab8c6aaa4926a120726
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-04 13:32:19 +0800

    Update ControlsSettingState.lua

--- data/options/ControlsSettingState.lua
+++ data/options/ControlsSettingState.lua
@@ -13,33 +13,33 @@ function addControls()
 end
 
 function inRoom()
-   nameRoom(controls setting)
+   nameRoom('controls setting')
 end
 
 function onCreate()
-   bg = create.bg.sprite(0,0,menu)
+   bg = create.bg.sprite(0, 0, 'menu')
    bg.scale(2.3,2.3)
 end
 
 function setting()
-   controls.up.text(keyboard.change)
-   controls.left.text(keyboard.change)
-   controls.right.text(keyboard.change)
-   controls.down.text(keyboard.change)
-   controls.A.text(keyboard.change)
-   controls.B.text(keyboard.change)
-   controls.C.text(keyboard.change)
-   controls.S.text(keyboard.change)
+   controls.up.text('keyboard.change')
+   controls.left.text('keyboard.change')
+   controls.right.text('keyboard.change')
+   controls.down.text('keyboard.change')
+   controls.A.text('keyboard.change')
+   controls.B.text('keyboard.change')
+   controls.C.text('keyboard.change')
+   controls.S.text('keyboard.change')
 end
 
 function onPress()
-   controls.color.press(255,0,0)
-   controls.color(255,255,255)
+   controls.color.press('255,0,0')
+   controls.color('255,255,255')
 end
 
 function exit()
-   get.change(data)
-   get.change(controls)
+   get.change('data')
+   get.change('controls')
    save()
    controls.change()
 end
