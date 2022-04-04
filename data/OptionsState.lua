6e3d73cbfb66b4e571aafd04715f96d915e2e7bd
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-04 13:29:30 +0800

    Update OptionsState.lua

--- data/OptionsState.lua
+++ data/OptionsState.lua
@@ -2,7 +2,7 @@ controls.load()
 shaders.camera()
 
 function inRoom()
-   nameRoom(options)
+   nameRoom('options')
 end
 
 function onUpdate()
@@ -17,31 +17,31 @@ function addControls()
 end
 
 function onCreate()
-   bg = create.bg.sprite(0,0,menu)
+   bg = create.bg.sprite(0, 0, 'menu')
    bg.scale(2.3,2.3)
-   options.list(Controls Setting,Game Setting)
+   options.list('Controls Setting','Game Setting')
    if debug then
-     options.list.add(Debug Setting)
+     options.list.add('Debug Setting')
    end
    if not debug then
-     options.list.remove(Debug Setting)
+     options.list.remove('Debug Setting')
    end
-   options = create.options.text(0,0[next.y-20],options.list,sans)
+   options = create.options.text(0, 0[next.y-20], options.list, 'sans')
 end
 
 function onPress()
    if Press Controls Setting then
      black.screen()
-     setRoom(controls setting)
+     setRoom('controls setting')
    end
    if Press Game Setting then
      black.screen()
-     setRoom(game setting)
+     setRoom('game setting')
    end
    if debug then
       if Press Debug Setting then
         black.screen()
-        setRoom(debug setting)
+        setRoom('debug setting')
       end
    end
 end
