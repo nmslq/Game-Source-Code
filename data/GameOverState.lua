29352091c1cfaadda22188dcd8fbdbecae02d778
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-04 13:23:54 +0800

    Update GameOverState.lua

--- data/GameOverState.lua
+++ data/GameOverState.lua
@@ -2,32 +2,33 @@ controls.load()
 shaders.camera()
 
 function inRoom()
-   nameRoom(die)
+   nameRoom('die')
 end
 
 function onUpdate()
    controls.Update()
+   shaders.Update()
 end
 
 function firstDie()
-   game = create.game.sprite(0, -150, game, 50, 30)
+   game = create.game.sprite(0, -150, 'game')
    game.alpha = 0
    game.alpha = game.alpha + 1
-   over = create.game.sprite(0, -100, over, 50, 30)
+   over = create.game.sprite(0, -100, 'over')
    over.alpha = 0
    over.alpha = over.alpha + 1
-   fd = create.fd.text(0, 0, you died!, 30, sans)
-   fd1 = create.fd1.text(0, 0, dont give up, 30, sans, 0)
+   fd = create.fd.text(0, 0, 'you died!', 'sans')
+   fd1 = create.fd1.text(0, 0, 'dont give up', 'sans')
 end
 
 function Die()
-   game = create.game.sprite(0, -150, game, 50, 30)
+   game = create.game.sprite(0, -150, 'game')
    game.alpha = 0
    game.alpha = game.alpha + 1
-   over = create.game.sprite(0, -100, over, 50, 30)
+   over = create.game.sprite(0, -100, 'over')
    over.alpha = 0
    over.alpha = over.alpha + 1
-   die = create.die.text(0, 0, ..., 30, sans)
+   die = create.die.text(0, 0, '...', sans)
 end
 
 function addControls()
@@ -37,5 +38,5 @@ function addControls()
 end
 
 function onPress()
-   setRoom(menu)
+   setRoom('menu')
 end
