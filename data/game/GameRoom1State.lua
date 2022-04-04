baba16f82e6bb908b10e6c9264dd08e19875a51d
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-04 13:35:28 +0800

    Update and rename GameRoom1.lua to GameRoom1State.lua

--- data/game/GameRoom1State.lua
+++ data/game/GameRoom1State.lua
@@ -6,7 +6,7 @@ stage = 1
 level = 1
 
 function inRoom()
-   nameRoom(game.1)
+   nameRoom('game.1')
 end
 
 function onUpdate()
@@ -18,8 +18,8 @@ end
 
 function summon()
    if time % 13 == 0 then
-      rx = math.random(stage.x)
-      ry = math.random(stage.y)
+      rx = math.random('stage.x')
+      ry = math.random('stage.y')
       summon.bullet(rx,ry)
    end
 end
