44d7b807db6d306a104e88903ec9ded5633d48e2
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-07 21:03:57 +0800

    Update StageData.lua

--- data/libraries/StageData.lua
+++ data/libraries/StageData.lua
@@ -2,15 +2,15 @@ function stages()
    if stage == 1 then
      gr = create.gr.sprite(0,-100,'stages/ground')
      gr.scale(1.6, 1.6)
-     gr.scoll(1, 1)
+     gr.setScrollFactor(1, 1)
      gr.set.camera('CameraGame')
      pf1 = create.pf1.sprite(-200,100,'stages/platform1')
      pf1.scale(1.3,1.3)
-     pf1.scoll(1,1)
+     pf1.setScrollFactor(1,1)
      pf1.set.camera('CameraGame')
      sky = create.sky.sprite(0,0,'stages/sky')
      sky.scale(3.4,3.4)
-     sky.scoll(0,0)
+     sky.setScrollFactor(0,0)
      sky.set.camera('CameraGame')
    end
 end
