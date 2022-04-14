11bcc5bdd2382f344960e1d38931dd506ae0efdb
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-14 14:09:11 +0800

    Create GameRoom2State.lua

---
+++ data/game/GameRoom2State.lua
@@ -0,0 +1,32 @@
+controls.load()
+fight.load()
+timer.load()
+shaders.camera()
+stage = 2
+level = 2
+
+function inRoom()
+   nameRoom('game2')
+end
+
+function onUpdate()
+  controls.Update()
+  fight.Update()
+  shaders.Update()
+  timer.Update()
+end
+
+function onCreate()
+   complate.xy(600,700)
+end
+
+function onComplate()
+    black.screen()
+    setRoom('game3')
+end
+
+function addControls()
+   if android then
+      addAndroidControls(full, B, C, S)
+   end
+end
