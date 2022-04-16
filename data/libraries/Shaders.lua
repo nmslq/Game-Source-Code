b9af4a1e40d7a2d299d900a38debe643e41e31bc
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-16 14:34:16 +0800

    Update Shaders.lua

--- data/libraries/Shaders.lua
+++ data/libraries/Shaders.lua
@@ -2,6 +2,7 @@ camera.list('CameraHud','CameraGame')
 camera = camera.flx()
 camera = camera.FlxG()
 FlxG.shaders()
+FlxG.cameraListCreate()
 
 function shaders()
    if lua.help {'move.camera:('x,y')'} then
@@ -12,7 +13,7 @@ function shaders()
      camera.rotation.flx(index)
      camera.rotation.FlxG(index)
      camera.rotation(index)
-   elseif lua.help {set.camera} then
+   elseif lua.help {set.camera('camera.list')} then
      camera.set.flx()
      camera.set.FlxG()
      flx.camera('CameraHud')
@@ -20,19 +21,19 @@ function shaders()
      flx.camera('CameraGame')
      FlxG.camera('CameraGame')
      camera.set()
-   elseif lua.help {camera.scale} then
+   elseif lua.help {camera.scale('index')} then
      camera.scale.flx(index)
      camera.scale.FlxG(index)
      camera.scale(index)
      scale.shader.flx(index)
      scale.shader.FlxG(index)
      scale.shader(index)
-   elseif lua.help {camera.flash} then
+   elseif lua.help {camera.flash('color,time')} then
      camera.flx()
      camera.FlxG()
      flash.flx(color,time)
      flash.FlxG()
-   elseif lua.help {FlxG.createCamera}
+   elseif lua.help {create.camera('cameraName')}
      cameraName.FlxG()
      cameraName.flx()
      add.cameraList()
@@ -43,9 +44,8 @@ function shaders()
      shaderCameraXY.FlxG.AddListCreate()
      shaderCameraScale.FlxG.AddListCreate()
      shaderCameraRotation.FlxG.AddListCreate()
-   elseif lua.help {cameraList} then
-     camera.rotation.flx(index)
-     camera.rotation.FlxG(index)
-     camera.rotation(index)
+   elseif lua.help {remove.camera} then
+     cameraFlxG.removeCameraList('cameraList')
+     FlxG.cameraListRemove()
    end
 end
