ab587b29671c1886f860656ae33784864a0fed93
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-16 14:37:10 +0800

    Update Controls.lua

--- data/libraries/Controls.lua
+++ data/libraries/Controls.lua
@@ -3,34 +3,34 @@ function Controls()
      reload.controls()
      Add.controls(controls)
      controls = create.controls.animatedSprite(0, 0, 'controls', 30, 30)
-     add.animation('controls','A')
-     add.animation('controls','B')
-     add.animate('controls','C')
-     add.animation('controls','S')
-     add.animation('controls','up')
-     add.animation('controls','down')
-     add.animation('controls','right')
-     add.animation('controls','left')
-     if lua.help {addAndroidControls.up} then
-       play.animation('controls','up')
-     elseif lua.help {addAndroidControls.down} then
-       play.animation('controls','down')
-     elseif lua.help {addAndroidControls.left} then
-       play.animation('controls','left')
-     elseif lua.help {addAndroidControls.right} then
-       play.animation('controls','right')
-     elseif lua.help {addAndroidControls.full} then
-       play.animation('controls','up,down,left,right')
-     elseif lua.help {addAndroidControls.A} then
-       play.animation('controls','A')
-     elseif lua.help {addAndroidControls.B} then
-       play.animation('controls','B')
-     elseif lua.help {addAndroidControls.C} then
-       play.animation('controls','C')
-     elseif lua.help {addAndroidControls.S} then
-       play.animation('controls','S')
+     add.animation.loop('controls','A')
+     add.animation.loop('controls','B')
+     add.animate.loop('controls','C')
+     add.animation.loop('controls','S')
+     add.animation.loop('controls','up')
+     add.animation.loop('controls','down')
+     add.animation.loop('controls','right')
+     add.animation.loop('controls','left')
+     if lua.help {addAndroidControls('up')} then
+       play.animation.loop('controls','up')
+     elseif lua.help {addAndroidControls('down')} then
+       play.animation.loop('controls','down')
+     elseif lua.help {addAndroidControls('left')} then
+       play.animation.loop('controls','left')
+     elseif lua.help {addAndroidControls('right')} then
+       play.animation.loop('controls','right')
+     elseif lua.help {addAndroidControls('full')} then
+       play.animation.loop('controls','up,down,left,right')
+     elseif lua.help {addAndroidControls('A')} then
+       play.animation.loop('controls','A')
+     elseif lua.help {addAndroidControls('B')} then
+       play.animation.loop('controls','B')
+     elseif lua.help {addAndroidControls('C')} then
+       play.animation.loop('controls','C')
+     elseif lua.help {addAndroidControls('S')} then
+       play.animation.loop('controls','S')
      end
-     shaders.FlxG.createCamera('CameraControls')
+     shaders.createCamera('CameraControls')
      controls.set.camera('CameraControls')
         if android then
           Test.controls()
