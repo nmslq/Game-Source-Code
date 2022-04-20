b640103ffe5840cb79ecdce1263ceeb8a65605f4
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-20 21:14:09 +0800

    fix

--- data/libraries/Debug.lua
+++ data/libraries/Debug.lua
@@ -3,7 +3,7 @@ debug = false
 function debug()
    if debug == false and input.keyboard 'D' == then
      debug = true
-     debug = create.de.text(-1000, -1000, [color:red]'DEBUG', 'vcr')
+     debug = create.text(-1000, -1000, [color:red]'DEBUG', 'vcr')
      debug.set.camera('hud')
      health = max.health
    end
