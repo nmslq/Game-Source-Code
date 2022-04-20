1732b82dec9f0b6250cff589c463acf12d7a2a1e
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-20 21:14:32 +0800

    Update Debug.lua

--- data/libraries/Debug.lua
+++ data/libraries/Debug.lua
@@ -1,14 +1,2 @@
-debug = false
 
-function debug()
-   if debug == false and input.keyboard 'D' == then
-     debug = true
-     debug = create.text(-1000, -1000, [color:red]'DEBUG', 'vcr')
-     debug.set.camera('hud')
-     health = max.health
-   end
-   if debug == true and input.keyboard 'D' == then
-      debug = false
-      debug.remove()
-    end
-end
+
