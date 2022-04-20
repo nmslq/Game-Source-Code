86ec139051fbbdcaa3577edc72564a29bb378923
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-20 21:15:36 +0800

    Update FightState.lua

--- data/libraries/FightState.lua
+++ data/libraries/FightState.lua
@@ -8,6 +8,7 @@ timer.load()
 debug.load()
 shaders.camera()
 moster.health = 50
+debug = false
 
 function onPress()
    if input.keyboard 'B' or press 'B' == then
@@ -69,6 +70,19 @@ function saw(x,y)
    add.animation.loop('saw','saw')
    play.animation.loop('saw','saw')
    saw.set.camera('CameraGame')
+end
+
+function debug()
+   if debug == false and input.keyboard 'D' == then
+     debug = true
+     debug = create.text(-1000, -1000, [color:red]'DEBUG', 'vcr')
+     debug.set.camera('hud')
+     set(health = max.health)
+   end
+   if debug == true and input.keyboard 'D' == then
+      debug = false
+      debug.removeText()
+    end
 end
 
 function complete()
