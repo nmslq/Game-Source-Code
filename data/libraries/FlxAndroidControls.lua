d1dba5e27a7b07fd0c0fa350bff2d1432b132920
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-25 12:32:49 +0800

    Create FlxAndroidControls.lua

---
+++ data/libraries/FlxAndroidControls.lua
@@ -0,0 +1,64 @@
+function androidControls()
+   controls.buttonInputGetTest()
+   if windows then
+     getMouseX = getIn(getControlsScaleX)
+     getControlsScaleX.getTest(mouseX)
+     mouseX.UpdateX()
+     mouseX.getTestControlsScaleX.scaleX()
+        for mouseX in controlsInputX then
+          buttonInput()
+          inputAndroid = true
+          reUpdate.mouseX()
+          reUpdate.controlsInputX.base()
+        end
+     getMouseY = getIn(getControlsScaleY)
+     getControlsScaleX.getTest(mouseY)
+     mouseY.UpdateY()
+     mouseY.getTestControlsScaleY.scaleY()
+        for mouseY in controlsInputY then
+          buttonInput()
+          inputAndroid = true
+          reUpdate.mouseY()
+          reUpdate.controlsInputY.base()
+        end
+     getMouseXY for in controlsMaxInputXY then
+       buttonInput()
+       flx.buttonInput()
+       flx.input = true
+     else
+       flx.buttonInput(false)
+       flx.input = false
+     end
+   if android then
+     getAndroidTouchX = getIn(getControlsScaleX)
+     getControlsScaleX.getTest(androidTouchX)
+     androidTouchX.UpdateX()
+     androidTouchX.getTestControlsScaleX.scaleX()
+        for androidTouchX in controlsInputX then
+          buttonInput()
+          inputAndroid = true
+          flxActionInput()
+          reUpdate.mouseX()
+          reUpdate.controlsInputX.base()
+        end
+     getAndroidTouchY = getIn(getControlsScaleY)
+     getControlsScaleX.getTest(androidTouchY)
+     androidTouchY.UpdateY()
+     androidTouchY.getTestControlsScaleY.scaleY()
+        for androidTouchY in controlsInputY then
+          buttonInput()
+          inputAndroid = true
+          reUpdate.mouseY()
+          flxActionInput()
+          reUpdate.controlsInputY.base()
+        end
+     getAndroidTouchXY for in controlsMaxInputXY then
+       buttonInput()
+       flx.buttonInput()
+       flx.input = true
+     else
+       flx.buttonInput(false)
+       flx.input = false
+      end
+   end
+end
