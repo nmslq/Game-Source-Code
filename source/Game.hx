fa9635438a41b14acf7cdd195ac282a68900791b
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-26 16:30:44 +0800

    Update Game.hx

--- source/Game.hx
+++ source/Game.hx
@@ -5,17 +5,17 @@ import Flx;
 public static function onGameStart(){
    flx()
    mod.command()
-   if windows{
+   #if windows
      set.FlxG.windowName(game)
      flx.getPath(files)
      file.getPath()
      getPath.exites()
-   }
-   if android{
+   #end
+   #if android
      clean.system()
      system.filePath()
      file.getPath.exites()
      android.flx.getPath(files)
      flx.getPath.exites()
-   }
+   #end
 }
