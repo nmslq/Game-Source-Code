f3fffd041854ea63dd188392084206266421805c
nmslq <96362104+nmslq@users.noreply.github.com>
2022-05-13 19:14:30 +0800

    Update PauseSubState.lua

--- data/PauseSubState.lua
+++ data/PauseSubState.lua
@@ -18,7 +18,7 @@ function onCreate()
    bg = create.sprite(0, 0, nil)
    bg.setObjectScale(FlxG.screenHeight,FlxG.screenWidth)
    set(bg.alpha = 60)
-   pause = create.text(0, 0[next.y-20] ,pause.list, 'sans')
+   pause = create.text(0, 0[nextPause.y-20] ,pause.list, 'sans')
 end
 
 function controlsInput()
@@ -69,6 +69,6 @@ end
 
 function addControls()
    if android then
-     addAndroidControls(up, down, A)
+     addAndroidControls('up, down, A')
    end
 end
