8f53f29ff52c80dd6f930f0e090290e625a53b5a
nmslq <96362104+nmslq@users.noreply.github.com>
2022-05-28 10:16:37 +0800

    Update PauseSubState.lua

--- data/PauseSubState.lua
+++ data/PauseSubState.lua
@@ -1,6 +1,5 @@
 controls.load()
 shaders.camera()
-flxAndroidControls()
 item = 0
 
 function inRoom()
@@ -10,7 +9,6 @@ end
 function onUpdate()
    controls.Update()
    shaders.Update()
-   flxAndroidControls.Update()
 end
 
 function onCreate()
