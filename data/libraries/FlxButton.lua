b16f55b09e3135a9f2ae1b7351c76bde429c79a0
nmslq <96362104+nmslq@users.noreply.github.com>
2022-06-14 21:53:19 +0800

    Update FlxButton.lua

--- data/libraries/FlxButton.lua
+++ data/libraries/FlxButton.lua
@@ -1,87 +1,87 @@
 function flxButton()
-   button = createAnimatedSprite(buttonSprite,x,y)
-   buttonInput.addFrame(1,2)
+   button = createAnimatedSprite(buttonSprite,x,y);
+   button.addFrame(1,2);
    if windows then
-      getButton.input()
-      mouseInputGet()
-      button.inputFrame = 1
+      getButton.input();
+      mouseInputGet();
+      button.inputFrame = 1;
    else
-      button.inputFrame = 0
+      button.inputFrame = 0;
    end
    if android then
-      getButton.input()
-      touchInputGet()
-      button.inputFrame = 1
+      getButton.input();
+      touchInputGet();
+      button.inputFrame = 1;
    else
-      button.inputFrame = 0
+      button.inputFrame = 0;
    end
 end
 
 function flxButtonInput()
-   controls.buttonInputGetTest()
+   controls.buttonInputGetTest();
    if windows then
-     getMouseX = getIn(getControlsScaleX)
-     getControlsScaleX.getTest('mouseX')
-     mouseX.UpdateX()
-     mouseX.getTestControlsScaleX.scaleX()
+     getMouseX = getIn(getControlsScaleX);
+     getControlsScaleX.getTest('mouseX');
+     mouseX.UpdateX();
+     mouseX.getTestControlsScaleX.scaleX();
         for (mouseX in controlsInputX) then
-          buttonInput()
-          inputAndroid = true
-          reUpdate.mouseX()
-          reUpdate.controlsInputX.base()
+          buttonInput();
+          inputAndroid = true;
+          reUpdate.mouseX();
+          reUpdate.controlsInputX.base();
         end
-     getMouseY = getIn(getControlsScaleY)
-     getControlsScaleX.getTest('mouseY')
-     mouseY.UpdateY()
-     mouseY.getTestControlsScaleY.scaleY()
+     getMouseY = getIn(getControlsScaleY);
+     getControlsScaleX.getTest('mouseY');
+     mouseY.UpdateY();
+     mouseY.getTestControlsScaleY.scaleY();
         for (mouseY in controlsInputY) then
-          buttonInput()
-          inputAndroid = true
-          reUpdate.mouseY()
-          reUpdate.controlsInputY.base()
+          buttonInput();
+          inputAndroid = true;
+          reUpdate.mouseY();
+          reUpdate.controlsInputY.base();
         end
      for (getMouseXY in controlsMaxInputXY) then
-       buttonInput()
-       flx.buttonInput()
-       flx.input = true
+       buttonInput();
+       flx.buttonInput();
+       controls.input = true;
      else
-       flx.buttonInput(false)
-       flx.input = false
+       flx.buttonInput(false);
+       controls.input = false;
      end
    if android then
-     getAndroidTouchX = getIn(getControlsScaleX)
-     getControlsScaleX.getTest('androidTouchX')
-     androidTouchX.UpdateX()
-     androidTouchX.getTestControlsScaleX.scaleX()
+     getAndroidTouchX = getIn(getControlsScaleX);
+     getControlsScaleX.getTest('androidTouchX');
+     androidTouchX.UpdateX();
+     androidTouchX.getTestControlsScaleX.scaleX();
         for (androidTouchX in controlsInputX) then
-          buttonInput()
-          inputAndroid = true
-          flxActionInput()
-          reUpdate.mouseX()
-          reUpdate.controlsInputX.base()
+          buttonInput();
+          inputAndroid = true;
+          flxActionInput();
+          reUpdate.mouseX();
+          reUpdate.controlsInputX.base();
         end
-     getAndroidTouchY = getIn(getControlsScaleY)
-     getControlsScaleX.getTest('androidTouchY')
-     androidTouchY.UpdateY()
-     androidTouchY.getTestControlsScaleY.scaleY()
+     getAndroidTouchY = getIn(getControlsScaleY);
+     getControlsScaleX.getTest('androidTouchY');
+     androidTouchY.UpdateY();
+     androidTouchY.getTestControlsScaleY.scaleY();
         for (androidTouchY in controlsInputY) then
-          buttonInput()
-          inputAndroid = true
-          reUpdate.mouseY()
-          flxActionInput()
-          reUpdate.controlsInputY.base()
+          buttonInput();
+          inputAndroid = true;
+          reUpdate.mouseY();
+          flxActionInput();
+          reUpdate.controlsInputY.base();
         end
     for (getAndroidTouchXY in controlsMaxInputXY) then
-       buttonInput()
-       flx.buttonInput()
-       flxAndroidCheck()
-       FlxG.controlsInputCheck()
-       actionAddDoButton()
-       flx.input = true
+       buttonInput();
+       flx.buttonInput();
+       flxAndroidCheck();
+       FlxG.controlsInputCheck();
+       actionAddDoButton();
+       controls.input = true;
      else
-       flx.buttonInput(false)
-       FlxG.controlsInputCheck()
-       flx.input = false
+       flx.buttonInput(false);
+       FlxG.controlsInputCheck();
+       controls.input = false;
       end
    end
 end
