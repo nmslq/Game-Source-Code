f6a6d22d93ffc7501ef670bcc67d8b74c5ea86c3
nmslq <96362104+nmslq@users.noreply.github.com>
2022-06-19 11:54:44 +0800

    Update CreditsState.lua

--- data/CreditsState.lua
+++ data/CreditsState.lua
@@ -1,53 +1,53 @@
-controls.load()
-shaders.camera()
-item = 0
+controls.load();
+shaders.camera();
+item = 0;
 
 function inRoom()
-   nameRoom('credits')
+   nameRoom('credits');
 end
 
 function addControls()
    if android then
-     addAndroidControls('up, down, A, B')
+     addAndroidControls('up, down, A, B');
    end
 end
 
 function onUpdate()
-   controls.Update()
-   shaders.Update()
+   controls.Update();
+   camera.Update();
 end
 
 function controlsInput()
    if getControlsInput == 'A' then
-     browersUrl.viewOpen(creditsUrl)
+     browers.openURL(creditsUrl);
    end
    if getControlsInput == 'B' then
-     setRoom('menu')
+     setRoom('menu');
    end
    if getControlsInput == 'up' then
-     change.credits('up')
+     change.credits('up');
    end
    if getControlsInput == 'down' then
-     change.credits('down')
+     change.credits('down');
    end
 end
 
 function changeCredits()
    if 'up' then
-     item = item + 1
+     item = item + 1;
    end
    if 'down' then
-     item = item - 1
+     item = item - 1;
    end
 end
 
 function onCreate()
-   bg = createSprite(0,0,'menu')
-   setObjectScale('bg', FlxG.screenHeight,FlxG.screenWidth)
-   credits = createText(0,0,{'credits.text'})
-   setObjectFont('credits', 'sans')
-   icon = createSprite(-30,0,{'credits.icon'})
-   title = createText(0,30,{'credits.title'})
+   bg = createSprite(0,0,'menu');
+   setObjectScale('bg', FlxG.screenHeight,FlxG.screenWidth);
+   credits = createText(0,0,{'credits.text'});
+   setObjectFont('credits', 'sans');
+   icon = createSprite(-30,0,{'credits.icon'});
+   title = createText(0,30,{'credits.title'});
 end
 
 function credits()
