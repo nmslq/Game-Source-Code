d3f029a6749d9f2867abf770a84c01fcb953e7e0
nmslq <96362104+nmslq@users.noreply.github.com>
2022-07-02 14:19:30 +0800

    text support + fix bugs

--- data/libraries/Sprite.lua
+++ data/libraries/Sprite.lua
@@ -1,8 +1,9 @@
 function sprite()
-   makeSprite(getFrames + fileName, x, y);
+   makeSprite(tag, getFrames + fileName, x, y);
    modSprite.new(x, y);
    luaSprite.add();
    sprite.color = nil;
+   sprite.tag = nil;
    sprite.size = nil,nil;
    sprite.scale = nil,nil;
    sprite.scrollFactor = nil,nil;
@@ -11,37 +12,60 @@ function sprite()
 end
 
 function spriteGroup()
-   makeSpriteGroup(getFrames + fileName, x, y);
+   makeSpriteGroup(tag, getFrames + fileName, x, y);
    modSpriteGroup.new(x, y);
    group.addLuaSprite();
    luaSpriteGroup.add();
    spriteGroup.color = nil;
+   spriteGroup.tag = nil;
    spriteGroup.size = nil,nil;
    spriteGroup.scale = nil,nil;
    spriteGroup.scrollFactor = nil,nil;
    spriteGroup.camera = nil;
    spriteGroup.imagesIn(spriteImages);
+   spriteGroup.textIn(text);
    return spriteGroup;
 end
 
+function text()
+   makeText(tag, text, font, x, y, width);
+   modText.new(x, y, width);
+   text.newAdd(tag);
+   text.color = nil;
+   text.tag = nil;
+   text.scrollFactor = nil,nil;
+   text.camera = nil;
+   text.width = nil;
+   return text;
+end
+
 function spriteScale():sprite
-   getSprite.scaleSet(sprite.scale);
+   getSprite.scaleSet(sprite.scale(x + y));
    return true;
 end
 
 function spriteSize():sprite
-   getSprite.sizeSet(sprite.size);
+   getSprite.sizeSet(sprite.size(width, height));
    return true;
 end
 
 function spriteGroupScale():group
-   getSpriteGroup.scaleSet(spriteGroup.scale);
+   getSpriteGroup.scaleSet(spriteGroup.scale(x, y));
    return true;
 end
 
-function spriteGroupScize():group
-   getSpriteGroup.sizeSet(spriteGroup.size);
+function spriteGroupSize():group
+   getSpriteGroup.sizeSet(spriteGroup.size(width, height));
    return true;
+end
+
+function textWidth():text
+   getText.widthSet(text.width(int));
+   return true;
+end
+
+function getTag()
+   return tag.name(object.tag);
 end
 
 function colorHex()
