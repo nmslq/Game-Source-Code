96f505931918f3d50b184c4fa00fc5f42c416f07
nmslq <96362104+nmslq@users.noreply.github.com>
2022-07-02 14:39:01 +0800

    Update GameRoom1State.lua

--- data/game/GameRoom1State.lua
+++ data/game/GameRoom1State.lua
@@ -1,31 +1,29 @@
-controls.load()
-fight.load()
-shaders.camera()
-stage = 1
-level = 1
-dodgeMath = 0
+controls.load();
+fight.load();
+stage = 1;
+level = 1;
+dodgeMath = 0;
 
 function inRoom()
-   nameRoom('game1')
+   nameRoom('game1');
 end
 
 function onUpdate()
-   controls.Update()
-   fight.Update()
-   shaders.Update()
+   controls.Update();
+   fight.Update();
 end
 
 function onCreate()
-   complate.xy(500,500)
+   complate.xy(500,500);
 end
 
 function onComplate()
-    black.screen()
-    setRoom('game2')
+    black.screen();
+    setRoom('game2');
 end
 
 function addControls()
    if android then
-      addAndroidControls('full, B, C, S')
+      addAndroidControls('full, B, C, S');
    end
 end
