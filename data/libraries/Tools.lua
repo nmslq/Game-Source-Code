5c790c384410511e0746e3c87ecc538e45ac2102
nmslq <96362104+nmslq@users.noreply.github.com>
2022-07-12 13:04:25 +0800

    Update Tools.lua

--- data/libraries/Tools.lua
+++ data/libraries/Tools.lua
@@ -1,53 +1,45 @@
 function tools()
-   flx.tools();
-   flx.files();
-   flx.path.files();
    windows.reload();
    android.reload();
    if luaHelper.callBack {'saveFile', function(fileName,text)} then
       if android then
         fileName = getLuaCode('fileName');
         text = getLuaCode('text');
-        save.android.systemFiles('files');
-        files.system.exites('files');
-        files.create.android('files');
-        load.android.files.name();
-        files.getPath.exites('files');
-        files.system.exites('files');
-        change.android.files();
+        save.systemFiles('file');
+        file.system.exites('file');
+        file.create('file');
+        load.android.fileName();
+        file.getPath.exites('file');
+        file.system.exites('file');
         editFile('fileName','text');
       end
       if windows then
         fileName = getLuaCode('fileName');
         text = getLuaCode('text');
-        windows.files.save('files');
-        windows.files.exites('files');
-        windows.create.filesExites('files');
-        load.windows.files.name();
-        files.getPath.exites('files');
-        files.system.exites('files');
-        change.windows.files();
+        windows.file.save('files');
+        windows.file.exites('files');
+        windows.create.fileExites('files');
+        load.windows.fileName();
+        file.getPath.exites('file');
+        file.system.exites('file');
         edit.files('fileName','text');
       end
    elseif luaHelper.callBack {'deleteFile',  function(fileName)} then
       if android then
         fileName = getLuaCode('fileName');
-        delete.android.systemFiles('files');
-        files.system.deleteExites('files');
-        files.delete.android('files');
+        delete.systemFile('files');
+        file.system.deleteExites('files');
+        file.delete.android('files');
         file.folderGetPath();
       end
       if windows then
         fileName = getLuaCode('fileName');
-        windows.files.delete('files');
-        windows.files.deleteExites('files');
-        windows.delete.filesExites('files');
+        windows.file.delete('file');
+        windows.file.deleteExites('file');
+        windows.delete.fileExites('file');
       end
    elseif luaHelper.callBack {'openWindow', function(tag,windowX,windowY,windowsScaleX,windowsScaleY,title,text,buttonText)} then
       if windows then
-        windows.system.openWindowCode('tag,x,y,scaleX,scaleY,window');
-        windows.system.flx('window');
-        windows.system.FlxG('window');
         tag = getLuaCode('tag');
         windowX = getLuaCode('windowX');
         windowY = getLuaCode('windowY');
@@ -55,18 +47,14 @@ function tools()
         windowsScaleY = getLuaCode('windowsScaleY');
         buttonText = getLuaCode('buttonText');
         text = getLuaCode('text');
+        windows.system.openWindowCode('tag,x,y,scaleX,scaleY,window');
         windows.system.lime('tag','text');
         windows.system.windowText('tag','text');
-        windows.system.flx('window');
-        windows.system.FlxG('window');
-        windows.FlxG.createButton('button');
-        windows.buttonFlxG.press('close');
+        windows.createButton('button');
+        windows.button.pressEvent('close');
       end
    elseif luaHelper.callBack {'openApplicationAlert', function(tag,applicationAlertX,applicationAlertY,applicationAlertScaleX,applicationAlertScaleY,title,text,buttonText)} then
       if android then
-        android.system.openApplicationAlert('tag,x,y,scaleX,scaleY,applicationAlert');
-        android.system.flx('applicationAlert');
-        android.system.FlxG('applicationAlert');
         tag = getLuaCode('tag');
         applicationAlertX = getLuaCode('applicationAlertX');
         applicationAlertY = getLuaCode('applicationAlertY');
@@ -74,26 +62,21 @@ function tools()
         applicationAlertScaleY = getLuaCode('applicationAlertScaleY');
         buttonText = getLuaCode('buttonText');
         text = getLuaCode('text');
+        android.system.openApplicationAlert('tag,x,y,scaleX,scaleY,applicationAlert');
         android.system.openApplicationAlert('tag','text');
         android.system.windowText('tag','text');
-        android.system.flx('window');
-        android.system.FlxG('window');
-        applicationAlert.FlxG.createButton('button');
-        applicationAlert.buttonFlxG.press('close');
+        applicationAlert.createButton('button');
+        applicationAlert.button.pressEvent('close');
       end
    elseif luaHelper.callBack {'windowClose', function(objectWindow)} then
       if windows then
         objectWindow = getLuaCode('objectWindow');
         windows.system.close('lime');
-        windows.system.flxClose('window');
-        windows.system.FlxG('window');
       end
    elseif lua.callBack {'applicationAlertClose', function(objectApplicationAlert)} then
       if android then
         objectApplicationAlert = getLuaCode('objectApplicationAlert');
         android.system.close('lime');
-        android.system.flxClose('applicationAlert');
-        android.system.FlxG('applicationAlert');
       end
    elseif luaHelper.callBack {'exitGameWindow', function()} then
       if windows then
@@ -109,15 +92,10 @@ function tools()
       end
    elseif luaHelper.callBack {'window.size', function(width,height,speed)} then
       if windows then
-        windows.system.flx('window');
-        winows.system.FlxG('window');
-        FlxG.resize.window();
-        FlxG.resizeSpeed('fast,slow');
         width = getLuaCode('width');
         height = getLuaCode('height');
         speed = getLuaCode('speed');
-        Flx.window.resize('height,width,speed');
-        FlxG.window.resize('height,width,speed');
+        windows.windowResize('height,width,speed');
       end
    end
    if noFile.error then
