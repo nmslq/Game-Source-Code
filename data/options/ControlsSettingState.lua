aaa070b116c7eeef33ef02da36624d96f0c7c656
nmslq <96362104+nmslq@users.noreply.github.com>
2022-07-14 12:43:41 +0800

    Update ControlsSettingState.lua

--- data/options/ControlsSettingState.lua
+++ data/options/ControlsSettingState.lua
@@ -1,70 +1,68 @@
-controls.load()
-shaders.camera()
-item = 0
+controls.load();
+item = 0;
 
 function onUpdate()
-   controls.Update()
-   shaders.Update()
+   controls.Update();
 end
 
 function addControls()
    if android then
-     addAndroidControls('up,down,A,B')
+     addAndroidControls('up,down,A,B');
    end
 end
 
 function inRoom()
-   nameRoom('controls setting')
+   nameRoom('controls setting');
 end
 
 function onCreate()
-   bg = create.sprite(0, 0, 'menu')
-   bg.setObjectScale(FlxG.screenHeight,FlxG.screenWidth)
-   options = new.option(0, 0[nextSetting.y - 20], controlsOptions)
+   bg = create.sprite(0, 0, 'menu');
+   bg.setObjectScale(FlxG.screenHeight,FlxG.screenWidth);
+   options = new.option(0, 0[nextSetting.y - 20], controlsOptions);
 end
 
 function setting()
-   controls.up.option('keyboard.change')
-   controls.left.option('keyboard.change')
-   controls.right.option('keyboard.change')
-   controls.down.option('keyboard.change')
-   controls.A.option('keyboard.change')
-   controls.B.option('keyboard.change')
-   controls.C.option('keyboard.change')
-   controls.S.option('keyboard.change')
-   controlsOptionColor('FlxG.Black,getColorFromRGB["0,0,0"]')
+   controls.up.option('keyboard.change');
+   controls.left.option('keyboard.change');
+   controls.right.option('keyboard.change');
+   controls.down.option('keyboard.change');
+   controls.A.option('keyboard.change');
+   controls.B.option('keyboard.change');
+   controls.C.option('keyboard.change');
+   controls.S.option('keyboard.change');
+   controlsOption.setColor(getColorFromRGB[0,0,0]);
 end
 
 function controlsInput()
    if getControlsInput == 'A' and press change.anyKey then
-     controlsOptionColor.press('FlxG.Red,getColorFromRGB["255,0,0"]')
-     controlsKeyChange()
+     controlsOptionPress.setColor(getColorFromRGB[255,0,0]);
+     controlsKeyChange();
    end
    if getControlsInput == 'B' == then
-     exit()
+     exit();
    end
    if getControlsInput == 'up' then
-     change.option('up')
+     change.option('up');
    end
    if getControlsInput == 'down' then
-     change.option('down')
+     change.option('down');
    end
 end
 
 function changeOption()
    if 'up' then
-     item = item + 1
+     item = item + 1;
    end
    if 'down' then
-     item = item - 1
+     item = item - 1;
    end
 end
 
 function exit()
-   get.change('data')
-   get.change('controls')
-   save()
-   controls.change()
-   black.screen()
-   setRoom('options')
+   get.change('data');
+   get.change('controls');
+   save();
+   controls.change();
+   black.screen();
+   setRoom('options');
 end
