5de48e7db2bfb37382572d09b24a0f65777716f4
nmslq <96362104+nmslq@users.noreply.github.com>
2022-07-14 12:45:05 +0800

    Update DebugSettingState.lua

--- data/options/DebugSettingState.lua
+++ data/options/DebugSettingState.lua
@@ -1,59 +1,59 @@
-controls.load()
-shaders.camera()
+controls.load();
+item = 0;
 
 function onUpdate()
-   controls.Update()
-   shaders.Update()
+   controls.Update();
+   shaders.Update();
 end
 
 function addControls()
    if android then
-     addAndroidControls('up,down,A,B')
+     addAndroidControls('up,down,A,B');
    end
 end
 
 function onCreate()
-   bg = create.sprite(0, 0, 'menu')
-   bg.setObjectScale(FlxG.screenHeight,FlxG.screenWidth)
-   options = new.option(0, 0[nextSetting.y - 20], setting.list)
+   bg = create.sprite(0, 0, 'menu');
+   bg.setObjectScale(game.screenHeight,game.screenWidth);
+   options = new.option(0, 0[nextSetting.y - 20], setting.list);
 end
 
 function setting()
-   setting.list('FPS Show','Player XY')
-   fps.show.list(false,true)
-   fps.show = true
-   player.xy.list(false,true)
-   player.xy = true
-   fps.text(system.fps)
-   player.text('game.playerGetXY')
+   setting.list('FPS Show','Player XY');
+   fps.show.list(false,true);
+   fps.show = true;
+   player.xy.list(false,true);
+   player.xy = true;
+   fps.text(system.fps);
+   player.text('game.playerGetXY');
 end
 
 function exit()
-   get.change('data')
-   get.change('setting')
-   save()
-   setting.change()
-   black.screen()
-   setRoom('options')
+   get.change('data');
+   get.change('setting');
+   save();
+   setting.change();
+   black.screen();
+   setRoom('options');
 end
 
 function controlsInput()
    if getControlsInput == 'B' == then
-     exit()
+     exit();
    end
    if getControlsInput == 'up' == then
-     change.option('up')
+     change.option('up');
    end
    if getControlsInput == 'down' == then
-     change.option('down')
+     change.option('down');
    end
 end
 
 function changeOption()
    if 'up' then
-     item = item + 1
+     item = item + 1;
    end
    if 'down' then
-     item = item - 1
+     item = item - 1;
    end
 end
